c885a8e6eb071c62406555e11681e1ec46ece117
redcode <manuel@zxe.io>
2018-10-09 01:07:46 +0200

    Project structure.

--- building/premake4.lua
+++ building/premake4.lua
@@ -8,7 +8,7 @@ solution "Z80"
 		language "C"
 		flags {"ExtraWarnings"}
 		files {"../sources/**.c"}
-		includedirs {"../API/C"}
+		includedirs {"../API"}
 		--buildoptions {"-std=c89 -pedantic -Wall"}
 
 		configuration "Release*"
