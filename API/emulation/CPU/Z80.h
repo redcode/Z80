7a36f872f307e24639bbee353129174382f94166
redcode <manuel@zxe.io>
2018-10-30 18:14:24 +0100

    GPLv3 headers.

--- API/emulation/CPU/Z80.h
+++ API/emulation/CPU/Z80.h
@@ -5,7 +5,18 @@
 \ \__/\_\ \_____\ \_\ \_\ \_\ \____\
  \/_/\/_/\/_____/\/_/\/_/\/_/\/____/
 Copyright (C) 1999-2018 Manuel Sainz de Baranda y Goñi.
-Released under the terms of the GNU General Public License v3. */
+
+This library  is free software: you  can redistribute it and/or  modify it under
+the terms of  the GNU General Public  License as published by  the Free Software
+Foundation,  either version  3 of  the License,  or (at  your option)  any later
+version.
+
+This library is distributed in the hope  that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty  of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should  have received a  copy of the GNU  General Public License  along with
+this library. If not, see <http://www.gnu.org/licenses/>. */
 
 #ifndef _emulation_CPU_Z80_H_
 #define _emulation_CPU_Z80_H_
