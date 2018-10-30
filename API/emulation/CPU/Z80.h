0c47286049e34f2fad77606bebdd302386676d43
redcode <manuel@zxe.io>
2018-10-30 18:11:00 +0100

    Dependency optionality.

--- API/emulation/CPU/Z80.h
+++ API/emulation/CPU/Z80.h
@@ -10,8 +10,11 @@ Released under the terms of the GNU General Public License v3. */
 #ifndef _emulation_CPU_Z80_H_
 #define _emulation_CPU_Z80_H_
 
-#include <Z/hardware/CPU/architecture/Z80.h>
-#include <Z/ABIs/generic/emulation.h>
+#ifdef CPU_Z80_DEPENDENCIES_H
+#	include CPU_Z80_DEPENDENCIES_H
+#else
+#	include <Z/hardware/CPU/architecture/Z80.h>
+#endif
 
 typedef struct {
 	zusize	  cycles;
@@ -31,16 +34,6 @@ typedef struct {
 
 Z_C_SYMBOLS_BEGIN
 
-#ifndef CPU_Z80_ABI
-#	ifdef CPU_Z80_STATIC
-#		define CPU_Z80_ABI
-#	else
-#		define CPU_Z80_ABI Z_API
-#	endif
-#endif
-
-CPU_Z80_ABI extern ZCPUEmulatorABI const abi_emulation_cpu_z80;
-
 #ifndef CPU_Z80_API
 #	ifdef CPU_Z80_STATIC
 #		define CPU_Z80_API
@@ -56,5 +49,25 @@ CPU_Z80_API void   z80_nmi  (Z80 *object);
 CPU_Z80_API void   z80_int  (Z80 *object, zboolean state);
 
 Z_C_SYMBOLS_END
+
+#ifndef CPU_Z80_OMIT_ABI_PROTOTYPE
+
+#	include <Z/ABIs/generic/emulation.h>
+
+	Z_C_SYMBOLS_BEGIN
+
+#	ifndef CPU_Z80_ABI
+#		ifdef CPU_Z80_STATIC
+#			define CPU_Z80_ABI
+#		else
+#			define CPU_Z80_ABI Z_API
+#		endif
+#	endif
+
+	CPU_Z80_ABI extern ZCPUEmulatorABI const abi_emulation_cpu_z80;
+
+	Z_C_SYMBOLS_END
+
+#endif
 
 #endif /* _emulation_CPU_Z80_H_ */
