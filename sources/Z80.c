8d2bcbb1153334abcb7356ced7fdd4bb3bee4dbb
redcode <manuel@zxe.io>
2018-11-01 04:59:12 +0100

    GPLv3 headers.

--- sources/Z80.c
+++ sources/Z80.c
@@ -6,7 +6,7 @@ Zilog \/_____/ \/_____/ \/_____/ CPU Emulator
 Copyright (C) 1999-2018 Manuel Sainz de Baranda y Goñi.
 
 This library  is free software: you  can redistribute it and/or  modify it under
-the terms of  the GNU General Public  License as published by  the Free Software
+the terms of  the GNU General Public License  as published by  the Free Software
 Foundation,  either version  3 of  the License,  or (at  your option)  any later
 version.
 
