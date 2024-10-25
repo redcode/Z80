7e9314fd8dfa994f92972be5bfd7ef4b6745dbe1
redcode <manuel@zxe.io>
2024-10-25 03:12:18 +0200

    Sphinx configutation.

--- documentation/conf.py
+++ documentation/conf.py
@@ -1,53 +1,15 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-# -- Project information -----------------------------------------------------
 
 author = u'Manuel Sainz de Baranda y Goñi'
 copyright = u'1999-2024 Manuel Sainz de Baranda y Goñi'
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = ['sphinx.ext.autosectionlabel', 'breathe']
-
-# sphinx.ext.autosectionlabel
-autosectionlabel_prefix_document = True
-
-# Breathe
-breathe_domain_by_extension = {'h': 'c'}
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+autosectionlabel_prefix_document = True
+breathe_domain_by_extension = {'h': 'c'}
 #html_theme = 'haiku'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+latex_elements = {'releasename': 'Version'}
