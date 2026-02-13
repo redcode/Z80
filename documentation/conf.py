f2337bb042faaf2b694a3894d684cf444e5403bd
redcode <manuel@zxe.io>
2026-02-13 21:33:50 +0100

    Build system improvements.

--- documentation/conf.py
+++ documentation/conf.py
@@ -3,13 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-author = u'Manuel Sainz de Baranda y Goñi'
-copyright = u'1999-2026 Manuel Sainz de Baranda y Goñi'
 extensions = ['sphinx.ext.autosectionlabel', 'breathe']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 autosectionlabel_prefix_document = True
 breathe_domain_by_extension = {'h': 'c'}
 #html_theme = 'haiku'
-html_static_path = ['_static']
+html_static_path = []
 latex_elements = {'releasename': 'Version'}
